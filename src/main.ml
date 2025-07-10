8c5220249b9978a04257da13a982f506f7472c48
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-07-10 21:01:35 +0530

    [This program now responds to multiple pings.]

--- src/main.ml
+++ src/main.ml
@@ -13,10 +13,10 @@ let () =
   (* Uncomment this block to pass the first stage *)
    let (client_socket, _) = accept server_socket in 
    let buff = Bytes.create 1024 in
-   let n = read client_socket buff_read 0 1024 in
+   let n = read client_socket buff 0 1024 in
    if n > 0 then begin
     let buff_write = Bytes.create n in
-    Bytes.blit buff_read 0 buff_write 0 n;
+    Bytes.blit buff 0 buff_write 0 n;
     write client_socket buff_write 0 n;
    end;
    close client_socket; 
