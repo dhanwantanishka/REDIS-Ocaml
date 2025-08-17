db90ccaad557c521795a3d12d46b72fef05e5295
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-08-17 16:19:56 +0530

    [This is an update to add support for the redis server to handle multiple clients along with multiple commands at the same time.]

--- src/main.ml
+++ src/main.ml
@@ -1,39 +1,86 @@
-open Unix
+open Eio
 
-let write_str fd s =
-  let buf = Bytes.of_string s in
-  let pos = 0 in
-  let len = Bytes.length buf in
-  write fd buf pos len
+type resp_value =
+  | SimpleString of string
+  | Error of string
+  | Integer of int
+  | BulkString of string option
+  | Array of resp_value list option
+
+exception Parse_error of string
+
+let rec parse_resp_value buffer = function
+  | '+' -> parse_simple_string buffer
+  | '-' -> parse_error buffer
+  | ':' -> parse_integer buffer
+  | '$' -> parse_bulk_string buffer
+  | '*' -> parse_array buffer
+  | c -> raise (Parse_error ("Unknown RESP type: " ^ String.make 1 c))
+
+and parse_simple_string buffer = SimpleString (Buf_read.line buffer)
+and parse_error buffer = Error (Buf_read.line buffer)
+and parse_integer buffer = Integer (buffer |> Buf_read.line |> int_of_string)
+
+and parse_bulk_string buffer =
+  let len_line = Buf_read.line buffer in
+  let len = int_of_string len_line in
+  match len with
+  | -1 -> BulkString None
+  | 0 ->
+    let _ = Buf_read.line buffer in
+    BulkString (Some "")
+  | l ->
+    let content = Buf_read.take l buffer in
+    let _ = Buf_read.line buffer in
+    BulkString (Some content)
+
+and parse_array buffer =
+  let len_line = Buf_read.line buffer in
+  let len = int_of_string len_line in
+  if len = -1
+  then Array None
+  else (
+    let rec parse_elements acc remaining =
+      if remaining = 0
+      then List.rev acc
+      else (
+        let first_char = Buf_read.any_char buffer in
+        let element = parse_resp_value buffer first_char in
+        parse_elements (element :: acc) (remaining - 1))
+    in
+    let elements = parse_elements [] len in
+    Array (Some elements))
 ;;
 
-let read_str fd =
-  let buf_len = 4096 in
-  let buf = Bytes.create buf_len in
-  let len = read fd buf 0 buf_len in
-  Bytes.sub_string buf 0 len
+let parse_resp buffer =
+  try
+    let first_char = Buf_read.any_char buffer in
+    parse_resp_value buffer first_char
+  with
+  | End_of_file -> raise (Parse_error "Empty Input")
+;;
+
+let handle_client socket _addr =
+  let bufr = Buf_read.of_flow socket ~initial_size:1024 ~max_size:1_000_000 in
+  while true do
+    let _ = parse_resp bufr in
+    Flow.copy_string "+PONG\r\n" socket;
+    traceln "socket sent PONG"
+  done
+;;
+
+let main ~net ~addr =
+  Switch.run ~name:"server"
+  @@ fun sw ->
+  let listening_socekt = Net.listen net ~sw ~reuse_addr:true ~backlog:1000 addr in
+  Fiber.fork ~sw (fun () ->
+    Net.run_server
+      listening_socekt
+      handle_client
+      ~on_error:(traceln "Error handling connection: %a" Fmt.exn))
 ;;
 
 let () =
-  let server_socket = socket PF_INET SOCK_STREAM 0 in
-  setsockopt server_socket SO_REUSEADDR true;
-  bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
-  listen server_socket 1;
-  let client_socket, _ = accept server_socket in
-  let rec handle_client () =
-    try
-      let msg = read_str client_socket in
-      if String.length msg = 0
-      then (
-        close client_socket;
-        close server_socket)
-      else (
-        ignore (write_str client_socket "+PONG\r\n");
-        handle_client ())
-    with
-    | Unix_error (ECONNRESET, _, _) | End_of_file ->
-      close client_socket;
-      close server_socket
-  in
-  handle_client ()
+  Eio_posix.run
+  @@ fun env -> main ~net:(Stdenv.net env) ~addr:(`Tcp (Net.Ipaddr.V4.loopback, 6379))
 ;;
