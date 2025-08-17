952b496f30624bfa436e3631153811de90baafbe
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-08-17 16:23:19 +0530

    [This is a support for the redis server to handle multiple commands from multiple clients at once.]

--- src/main.ml
+++ src/main.ml
@@ -1,86 +1,47 @@
-open Eio
+open Unix
 
-type resp_value =
-  | SimpleString of string
-  | Error of string
-  | Integer of int
-  | BulkString of string option
-  | Array of resp_value list option
+let process_req _ _ =
+  let res = "+PONG\r\n" in
+  res
 
-exception Parse_error of string
-
-let rec parse_resp_value buffer = function
-  | '+' -> parse_simple_string buffer
-  | '-' -> parse_error buffer
-  | ':' -> parse_integer buffer
-  | '$' -> parse_bulk_string buffer
-  | '*' -> parse_array buffer
-  | c -> raise (Parse_error ("Unknown RESP type: " ^ String.make 1 c))
-
-and parse_simple_string buffer = SimpleString (Buf_read.line buffer)
-and parse_error buffer = Error (Buf_read.line buffer)
-and parse_integer buffer = Integer (buffer |> Buf_read.line |> int_of_string)
-
-and parse_bulk_string buffer =
-  let len_line = Buf_read.line buffer in
-  let len = int_of_string len_line in
-  match len with
-  | -1 -> BulkString None
-  | 0 ->
-    let _ = Buf_read.line buffer in
-    BulkString (Some "")
-  | l ->
-    let content = Buf_read.take l buffer in
-    let _ = Buf_read.line buffer in
-    BulkString (Some content)
-
-and parse_array buffer =
-  let len_line = Buf_read.line buffer in
-  let len = int_of_string len_line in
-  if len = -1
-  then Array None
-  else (
-    let rec parse_elements acc remaining =
-      if remaining = 0
-      then List.rev acc
-      else (
-        let first_char = Buf_read.any_char buffer in
-        let element = parse_resp_value buffer first_char in
-        parse_elements (element :: acc) (remaining - 1))
-    in
-    let elements = parse_elements [] len in
-    Array (Some elements))
-;;
-
-let parse_resp buffer =
+let handle_client client_socket =
   try
-    let first_char = Buf_read.any_char buffer in
-    parse_resp_value buffer first_char
+    let buffer = Bytes.create 1024 in
+    let bytes_read = ref (read client_socket buffer 0 1024) in
+    while !bytes_read > 0 do
+      let response = process_req buffer bytes_read in
+      let _ =
+        write client_socket (String.to_bytes response) 0
+          (String.length response)
+      in
+      bytes_read := read client_socket buffer 0 1024
+    done
   with
-  | End_of_file -> raise (Parse_error "Empty Input")
-;;
+  | Unix_error (ECONNRESET, _, _) -> ()
+  | End_of_file -> ()
 
-let handle_client socket _addr =
-  let bufr = Buf_read.of_flow socket ~initial_size:1024 ~max_size:1_000_000 in
-  while true do
-    let _ = parse_resp bufr in
-    Flow.copy_string "+PONG\r\n" socket;
-    traceln "socket sent PONG"
-  done
-;;
+let run_and_close client_socket =
+  let finally () = close client_socket in
+  let work () = handle_client client_socket in
+  Fun.protect ~finally work
 
-let main ~net ~addr =
-  Switch.run ~name:"server"
-  @@ fun sw ->
-  let listening_socekt = Net.listen net ~sw ~reuse_addr:true ~backlog:1000 addr in
-  Fiber.fork ~sw (fun () ->
-    Net.run_server
-      listening_socekt
-      handle_client
-      ~on_error:(traceln "Error handling connection: %a" Fmt.exn))
-;;
+let rec accept_loop server_socket =
+  let client_socket, _ = accept server_socket in
+  let _ = Thread.create run_and_close client_socket in
+  accept_loop server_socket
 
 let () =
-  Eio_posix.run
-  @@ fun env -> main ~net:(Stdenv.net env) ~addr:(`Tcp (Net.Ipaddr.V4.loopback, 6379))
-;;
+  (* You can use print statements as follows for debugging, they'll be visible when running tests. *)
+  Printf.eprintf "Logs from your program will appear here!\n";
+
+  (* Create a TCP server socket *)
+  let server_socket = socket PF_INET SOCK_STREAM 0 in
+  setsockopt server_socket SO_REUSEADDR true;
+  bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
+  listen server_socket 10;
+
+  (* Start handling requests *)
+  try accept_loop server_socket
+  with e ->
+    close server_socket;
+    raise e
